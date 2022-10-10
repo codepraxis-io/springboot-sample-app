4c4f701b4bc6ce8182bfb4492afe0cc147d9061b
griggheo <grig@gheorghiu.net>
2022-10-09 19:17:49 -0700

    simple maven app

---
+++ src/main/java/com/example/dockerdeepdive/DockerdeepdiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerdeepdive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerdeepdiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerdeepdiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dockerdeepdive/DockerdeepdiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerdeepdive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerdeepdiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
